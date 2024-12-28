55226b3ad6d4b25d6e04411efb66235e8c443fa1
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-12-28 12:09:03 +0530

    Fix the read/not read toggle and add comments.

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function Book (title, author, noOfPages, readStatus) {
 	this.readStatus = readStatus;
 }
 
+//Adds book to the library array.
 function addBookToLibrary(title, author, noOfPages, readStatus) {
 	const newBook = new Book(title, author, noOfPages, readStatus);
 	myLibrary.push(newBook);
 displayBooks(myLibrary);
 }
 
+//This displays all the books currently present in the array.
+//Creates a div and adds all the information about the book.
 function displayBooks(myLibrary) {
 	const container = document.querySelector('.container');
 	container.innerHTML = '';
@@ -40,13 +43,28 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, "Read");
 addBookToLibrary("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, "Read");
 addBookToLibrary("The Alchemist", "Paulo Coelho", 208, "Not Read");
 
+
+
+//This function handles the read/not read text of  buttons
+//and also their styling.
 const readButtons = document.querySelectorAll('.book-button');
 readButtons.forEach((button) => {
+	if(button.innerHTML === "Read") {
+		button.classList.add('read');
+	}
+	else {
+		button.classList.add('notread');
+	}
   button.addEventListener('click', () => {
     if (button.innerHTML === "Read") {
       button.innerHTML = "Not Read";
+		button.classList.add('notread');
+		button.classList.remove('read');
+
     } else {
       button.innerHTML = "Read";
+	  		button.classList.add('read');
+		button.classList.remove('notread');
     }
   });
 });
